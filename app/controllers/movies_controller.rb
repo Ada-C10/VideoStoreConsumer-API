5fdbcda6d7beacd794fca62b8f2269b115821e98
Amanda Ungco <amanda.ungco@gmail.com>
2018-12-18 15:31:14 -0800

    updated post method and attempted  delete: incomplete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :require_movie, only: [:show]
+  before_action :require_movie, only: [:show, :delete]
 
   def index
     if params[:query]
@@ -17,24 +17,41 @@ class MoviesController < ApplicationController
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
   end
-
-
-  def new
-  end 
 
   def create
-  end
 
+    movie = Movie.new(movie_params)
 
-  private
-
-  def require_movie
-    @movie = Movie.find_by(title: params[:title])
-    unless @movie
-      render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
+    if movie.save
+      render json: { id: movie.id }, status:  :ok
+    else
+      render json: {
+        errors: {
+          title: ["Could not create '#{movie_params[:title]}' Movie"]
+        },
+        message: movie.errors.messages
+        }, status: :bad_request
+      end
     end
+
+    def destroy
+    end
+
+
+    private
+
+    def require_movie
+      @movie = Movie.find_by(title: params[:title])
+      unless @movie
+        render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
+      end
+    end
+
+    def movie_params
+      params.permit(:title, :overview, :image_url, :release_date)
+    end
+
   end
-end
