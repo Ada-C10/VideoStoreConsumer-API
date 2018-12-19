ec631a489af792f662899300aaef4b1cc7ad0d96
Jane <jane.edwards.mcnear@gmail.com>
2018-12-19 10:40:28 -0800

    added overdue rental seeds

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,13 +10,10 @@ class Movie < ApplicationRecord
 
   def image_url
     orig_value = read_attribute :image_url
-    puts orig_value
     if !orig_value
       MovieWrapper::DEFAULT_IMG_URL
-    elsif external_id ##&& id < 100
+    elsif external_id
       MovieWrapper.construct_image_url(orig_value)
-    # elsif external_id && id > 100
-    #   MovieWrapper.construct_image_url_new(orig_value)
     else
       orig_value
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,15 @@ JSON.parse(File.read('db/seeds/movies.json')).each do |movie_data|
   movies.first.inventory = movie_data['inventory']
   movies.first.save unless movies.empty?
 end
+
+# Comment out "validate :due_date_in_future, on: :create" to seed DB
+
+JSON.parse(File.read('db/seeds/rentals.json')).each do |rental|
+  new_rental = Rental.new(rental)
+  customer = Customer.all.sample
+  movie = Movie.all.sample
+  new_rental.customer = customer
+  new_rental.movie = movie
+
+  new_rental.save
+end
