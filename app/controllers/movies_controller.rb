ecf603101a755e7a2a939247325e0ea698ee7da1
Shelan <shelandebesai@gmail.com>
2018-12-19 11:34:22 -0800

    merge

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,23 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
+  def create
+    movie = Movie.new(
+      title: params[“title”],
+      overview: params[“overview”],
+      release_date: params[“release_date”],
+      image_url: params[“image_url”], #(api_result[“poster_path”] ? self.construct_image_url(api_result[“poster_path”]) : nil),
+      external_id: params[“external_id”])
+
+    if movie.save
+     render json: movie.asjson(only: [:id]), status: :ok
+    else
+     render json: {
+       erros: movie.errors.messages
+       }, status: :bad_request
+     end
+  end
+ 
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
