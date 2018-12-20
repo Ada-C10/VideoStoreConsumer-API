6ea17420c27178e9923ddfa4af4aec69c28e57a7
Trang <trang.frego@gmail.com>
2018-12-20 10:37:44 -0800

    add validation for uniqueness on external_id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,22 +2,17 @@ class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
 
   def create
-    movie = Movie.new(
-      title: params[“title”],
-      overview: params[“overview”],
-      release_date: params[“release_date”],
-      image_url: params[“image_url”], #(api_result[“poster_path”] ? self.construct_image_url(api_result[“poster_path”]) : nil),
-      external_id: params[“external_id”])
+    movie = Movie.new(movie_params)
 
     if movie.save
-     render json: movie.asjson(only: [:id]), status: :ok
+      render json: movie.as_json(only: [:id]), status: :ok
     else
-     render json: {
-       erros: movie.errors.messages
-       }, status: :bad_request
-     end
+      render json: {
+        errors: movie.errors.messages
+      }, status: :bad_request
+    end
   end
- 
+
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
@@ -46,5 +41,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.permit(:title, :overview, :release_date, :image_url, :external_id)
   end
 end
