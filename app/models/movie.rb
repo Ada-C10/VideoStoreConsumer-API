8bff39c9a4443404455c7e4f99274945f129ad1e
Katrina Aganon <katrina.aganon@gmail.com>
2018-12-19 16:08:18 -0800

    update backend image_url working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
+
   def available_inventory
     self.inventory - Rental.where(movie: self, returned: false).length
   end
@@ -10,7 +11,7 @@ class Movie < ApplicationRecord
     orig_value = read_attribute :image_url
     if !orig_value
       MovieWrapper::DEFAULT_IMG_URL
-    elsif orig_value.length != 31
+    elsif orig_value !~ %r{\Ahttps?://}
       MovieWrapper.construct_image_url(orig_value)
     else
       orig_value
